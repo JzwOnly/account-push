1cdd34ee7d1badba50b325120bfeca68ec9021d8
JzwOnly <836579250@qq.com>
2023-07-19 13:57:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,62 +6,51 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx13330cedbe59d7b9',
-
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: 'f9863bd8e309dee47d7494cf8bca611b',
-
-  PROVINCE: '江西省',
-  CITY: '上饶市',
-
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '文琪宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'okB8l6RyODQV-dJYs-cSvGYWp6AA',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'GRz60GZEO3Ndyt96mGGvyvHPb7rGhu-o4SYOcF8MPzA',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '03-03',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '2000', date: '03-03',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '宝贝', year: '2000', date: '2-28',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2023', date: '05-14',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2023-07-16' },
-        // 结婚纪念日
-        // { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'AK65VwwXQCkYwnupSCUnBKSrH5Zh04H7nMMHqNr3MD4',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'okB8l6WuEdxK7yCmOOlQjMSibWc8',
-    }
-  ],
-
+  "APP_ID": "wx13330cedbe59d7b9",
+  "APP_SECRET": "f9863bd8e309dee47d7494cf8bca611b",
+  "IS_SHOW_COLOR": true,
+  "CALLBACK_TEMPLATE_ID": "AK65VwwXQCkYwnupSCUnBKSrH5Zh04H7nMMHqNr3MD4",
+  "CALLBACK_USERS": [{
+    "name": "自己",
+    "id": "okB8l6WuEdxK7yCmOOlQjMSibWc8"
+  }],
+  "USERS": [{
+    "name": "文琪宝贝",
+    "id": "okB8l6WuEdxK7yCmOOlQjMSibWc8",
+    "useTemplateId": "RUzkBIkzuC8kGXlIW1fxB3IyLKjSg-qBnRndXRaO7ns",
+    "province": "江西省",
+    "city": "上饶市",
+    "horoscopeDate": "03-03",
+    "horoscopeDateType": "今日",
+    "openUrl": "https://s.weibo.com/top/summary?cate=realtimehot",
+    "festivals": [{
+      "type": "生日",
+      "name": "生日",
+      "date": "03-03",
+      "year": "2000"
+    }, {
+      "type": "节日",
+      "name": "恋爱纪念日",
+      "date": "07-16",
+      "year": "2023"
+    }],
+    "customizedDateList": [
+      // 在一起的日子
+      { keyword: 'love_day', date: '2023-07-16' },
+    ]
+  }],
+  "SWITCH": {
+    "weather": true,
+    "holidaytts": true,
+    "CIBA": true,
+    "oneTalk": false,
+    "earthyLoveWords": true,
+    "momentCopyrighting": false,
+    "poisonChickenSoup": false,
+    "poetry": false,
+    "horoscope": true,
+    "birthdayMessage": true
+  }
 }
 
 module.exports = USER_CONFIG
-
