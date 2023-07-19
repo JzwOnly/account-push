7f0a7afa3b35661a3664d2f514f3ce21e748e89f
JzwOnly <836579250@qq.com>
2023-07-19 15:38:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,34 +14,61 @@ const USER_CONFIG = {
     "name": "自己",
     "id": "okB8l6WuEdxK7yCmOOlQjMSibWc8"
   }],
-  "USERS": [{
-    "name": "文琪宝贝",
-    "id": "okB8l6WuEdxK7yCmOOlQjMSibWc8",
-    "useTemplateId": "RUzkBIkzuC8kGXlIW1fxB3IyLKjSg-qBnRndXRaO7ns",
-    "province": "江西省",
-    "city": "上饶市",
-    "horoscopeDate": "03-03",
-    "horoscopeDateType": "今日",
-    "openUrl": "https://s.weibo.com/top/summary?cate=realtimehot",
-    "festivals": [{
-      "type": "生日",
-      "name": "宝贝",
-      "date": "03-03",
-      "year": "2000"
-    }, {
-      "type": "节日",
-      "name": "恋爱纪念日",
-      "date": "07-16",
-      "year": "2023"
-    }],
-    "customizedDateList": [
-      // 在一起的日子
-      { keyword: 'love_day', date: '2023-07-16' },
-    ]
-  }],
+  "USERS": [
+    {
+      "name": "文琪宝贝",
+      "id": "okB8l6RyODQV-dJYs-cSvGYWp6AA",
+      "useTemplateId": "RUzkBIkzuC8kGXlIW1fxB3IyLKjSg-qBnRndXRaO7ns",
+      "province": "江西省",
+      "city": "上饶市",
+      "horoscopeDate": "03-03",
+      "horoscopeDateType": "今日",
+      "openUrl": "https://s.weibo.com/top/summary?cate=realtimehot",
+      "festivals": [{
+        "type": "生日",
+        "name": "宝贝",
+        "date": "03-03",
+        "year": "2000"
+      }, {
+        "type": "节日",
+        "name": "恋爱纪念日",
+        "date": "07-16",
+        "year": "2023"
+      }],
+      "customizedDateList": [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-07-16' },
+      ]
+    },
+    {
+      "name": "自己",
+      "id": "okB8l6WuEdxK7yCmOOlQjMSibWc8",
+      "useTemplateId": "RUzkBIkzuC8kGXlIW1fxB3IyLKjSg-qBnRndXRaO7ns",
+      "province": "江西省",
+      "city": "上饶市",
+      "horoscopeDate": "02-14",
+      "horoscopeDateType": "今日",
+      "openUrl": "https://s.weibo.com/top/summary?cate=realtimehot",
+      "festivals": [{
+        "type": "生日",
+        "name": "自己",
+        "date": "02-14",
+        "year": "1995"
+      }, {
+        "type": "节日",
+        "name": "恋爱纪念日",
+        "date": "07-16",
+        "year": "2023"
+      }],
+      "customizedDateList": [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-07-16' },
+      ]
+    }
+  ],
   "SWITCH": {
     "weather": true,
-    "holidaytts": true,
+    "holidaytts": false,
     "CIBA": true,
     "oneTalk": false,
     "earthyLoveWords": true,
